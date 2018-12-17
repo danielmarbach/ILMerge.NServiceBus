138683ba63400e76ca35362cb68778c12b22d17d
danielmarbach <daniel.marbach@openplace.net>
2018-12-17 15:54:43 +0100

    Repro

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
+using NServiceBus;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var endpointConfiguration = new EndpointConfiguration("Samples.Endpoint.ILMerge");
+
+            // workaround to avoid 
+            //  Can't find any behaviors/connectors for the root context (NServiceBus.Pipeline.ITransportReceiveContext)
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            var methodInfo = endpointConfiguration.GetType()
+                .GetMethod("TypesToScanInternal", BindingFlags.Instance | BindingFlags.NonPublic);
+            methodInfo.Invoke(endpointConfiguration, new object[] { types });
+
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+            try
+            {
+                await Endpoint.Start(endpointConfiguration);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyEvent : IEvent { }
+
+    public class Handler : IHandleMessages<MyEvent>
+    {
+        public Task Handle(MyEvent message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
         }
     }
 }
